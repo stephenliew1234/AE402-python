84a826e5c841619bb8ad9f572d45ff99c4037d7d
stephenliew1234 <78331618+stephenliew1234@users.noreply.github.com>
2021-02-02 16:22:26 +0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,11 +1,9 @@
-score1=int(input("math score")
-score2=int(input("English score")
-if not((score1>=0 and score <=100)and(score2>=0and score2 <=100)):
-    print("please put 0 to 100 midlle score)
-elif(score >=90 and score2>=90 or score1 ==100 ot score2==100):
-    print("you got scholarship")
-else:
-    print("none")
-      
-    
+import turtle
+alex=turtle.Turtle()
+alex.shape("turtle")
+alex.penuo()
+for i in range(0,100,5):
+    alex.forward(30+i)
+    alex.left(30)
+    alex.stamp()
 
