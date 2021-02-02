12ff661f9ad02970ef152474849831cb5b2e7342
stephenliew1234 <78331618+stephenliew1234@users.noreply.github.com>
2021-02-02 10:38:53 +0800

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -2,5 +2,8 @@ import random
 num=random.randint(1,10)
 x=input("what do number do you want to choose")
 x=int(x)
-if (x=num):
+if (x==num):
     print("get correct")
+else:
+    print("wrong")
+    print("answer is num")
