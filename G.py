2d8990f83762fe3494629557e341f3ab242fc0cd
stephenliew1234 <78331618+stephenliew1234@users.noreply.github.com>
2021-02-02 11:48:04 +0800

    Add files via upload

---
+++ G.py
@@ -0,0 +1,17 @@
+import random
+count=0
+num=random.randint(1,10)
+while<5:
+  guess=int(input("guess the number))
+  count-count+1
+     print("get correct")
+     if num==guess:
+         print("correct")
+         print("count")
+    elif guess>num:
+         print("too big")
+         print("cpunt")
+    else:
+        print("too big")
+        print("count")
+        
