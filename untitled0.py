00ce83756e88a2dd005422e0ab26891e409bc5e0
stephenliew1234 <78331618+stephenliew1234@users.noreply.github.com>
2021-02-02 10:01:10 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+score1=int(input("math score")
+score2=int(input("English score")
+if not((score1>=0 and score <=100)and(score2>=0and score2 <=100)):
+    print("please put 0 to 100 midlle score)
+elif(score >=90 and score2>=90 or score1 ==100 ot score2==100):
+    print("you got scholarship")
+else:
+    print("none")
+      
+    
+
