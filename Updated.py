eb36e705b69cefc6045226d675e5227edb94e688
stephenliew1234 <78331618+stephenliew1234@users.noreply.github.com>
2021-02-03 14:01:09 +0800

    Add files via upload

---
+++ Updated.py
@@ -0,0 +1,27 @@
+n=int(input("how many people are in the class")
+score=[]
+name=[]
+lowest=100
+highest=0
+highestname=""
+lowestname=""
+summath=0
+for i in range(0,2*n):
+    mathname=(input("name"?))
+    score.append(mathscore)
+    print(score)
+    if i %2==1:
+        score[i]=int(score[i])
+    if hightest<mathscore:
+        hightest=mathscore
+        highestname=mathname
+        if hightest<mathscore:
+            hightest=mathscore
+            hightestname=mathname
+    if lowest>mathscore:
+       lowest=mathscore
+       lowestname=mathname
+     summath=summath+score[i]
+     print("highest,highestname")
+     print("lowest,Lowestname")
+     print(summath/n)   
